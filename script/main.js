696fedc4696429a0057dd166909af4e39d05eb5a
qingzhixing <qzsptmail@protonmail.com>
2023-05-11 22:23:22 +0800

    摸鱼去了,备份一下

---
+++ script/main.js
@@ -0,0 +1,7 @@
+function StartChangeTitleTextEmoji(firstEmojiId = 0, secondEmojiId=0) {
+    const emojiArray = ['🍕',]
+}
+window.onload = () => {
+    StartChangeTitleTextEmoji(0, 0);
+    console.log("@qingzhixing 2023-XX-XX");
+}
